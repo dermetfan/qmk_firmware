b8649a48c7aee800e45ac5af0073cb452397e49d
Robin Stumm <serverkorken@gmail.com>
2022-07-31 20:57:10 +0200

    keyboards/bastardkb/charybdis/4x6/keymaps/dermetfan-vial: add back shift key
    Accidentally removed in d4a680c15b.

--- keyboards/bastardkb/charybdis/4x6/keymaps/dermetfan-vial/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/dermetfan-vial/keymap.c
@@ -54,13 +54,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
           KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,       KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-        KC_TAB,    KC_Q,    KC_W,    KC_D,    KC_F,    KC_K,       KC_J,    KC_U,    KC_R,    KC_L, KC_SCLN, KC_BSLS,
+       KC_LGUI,    KC_Q,    KC_W,    KC_D,    KC_F,    KC_K,       KC_J,    KC_U,    KC_R,    KC_L, KC_SCLN, KC_BSLS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_BSPC,    KC_A,    KC_S,    KC_E,    KC_T,    KC_G,       KC_Y,    KC_N,    KC_I,    KC_O,    KC_H, KC_QUOT,
+        KC_TAB,    KC_A,    KC_S,    KC_E,    KC_T,    KC_G,       KC_Y,    KC_N,    KC_I,    KC_O,    KC_H, KC_QUOT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LGUI,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_P,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, PT_RCTL,
+       KC_BSPC,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_P,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, PT_RCTL,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                            LALT_T(KC_BSPC), LOWER, KC_LGUI,      RAISE,  KC_SPC,
+                            LALT_T(KC_BSPC), LOWER, KC_LSFT,      RAISE,  KC_SPC,
                                     LCTL_T(KC_ESC), POINTER,     KC_ENT
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
