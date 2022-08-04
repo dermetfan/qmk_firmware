476922b750306baf93e842990d99a71f71f26150
Robin Stumm <serverkorken@gmail.com>
2022-08-04 09:35:53 +0200

    keyboards/bastardkb/charybdis/4x6/keymaps/dermetfan-vial: disable auto pointer layer

--- keyboards/bastardkb/charybdis/4x6/keymaps/dermetfan-vial/config.h
+++ keyboards/bastardkb/charybdis/4x6/keymaps/dermetfan-vial/config.h
@@ -57,10 +57,8 @@
 /* Charybdis-specific features. */
 
 #ifdef POINTING_DEVICE_ENABLE
-// Automatically enable the pointer layer when moving the trackball.  See also:
-// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS`
-// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD`
-#    define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+// Automatically enable the pointer layer when moving the trackball.
+// #    define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 #    define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 750
 #    define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 1
 #endif // POINTING_DEVICE_ENABLE
